0fd8f9284843c20b9b83ddffdf62313aa123a073
Kostia Bulanovich <reveni@rambler.ru>
2021-07-06 16:40:42 +0400

    Initial commit

---
+++ network/src/ru/test1/nwtMessenger/network/TCPConnectionListener.java
@@ -0,0 +1,10 @@
+package ru.test1.nwtMessenger.network;
+
+public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+    void onReceiveString(TCPConnection tcpConnection, String value);
+    void onDisconnect(TCPConnection tcpConnection);
+    void onException(TCPConnection tcpConnection, Exception e);
+
+}
